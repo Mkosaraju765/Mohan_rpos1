26b0aaea60bc86d35545f11c748f46362fcf138d
Mohan K <85210832+Mkosaraju765@users.noreply.github.com>
2023-02-28 01:00:57 -0600

    Add files via upload

---
+++ Simple.java
@@ -0,0 +1,37 @@
+    package myPack;
+    public class Simple {
+
+        public static void main(String[] args) {
+            System.out.println("welcomes to packages");
+
+        }
+
+    }
+
+    //See this program in the git
+
+
+    //Compiling the java package 
+
+   // javac -d directory javafilename
+   /* javac -d Simple.java
+    * 
+
+    The -d switch the destination where to put the generated class file 
+    You can use the directory name like /home (in case of linux),
+    d:/abc( in case of the windows) etc 
+
+    */
+
+
+    /*
+    
+    accessign the package form another package
+    import package.*;
+    import package.classname;
+    fully qualified name 
+
+    
+
+   */
+
