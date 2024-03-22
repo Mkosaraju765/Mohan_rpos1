615205797b624500c9a15dc4d716adcbe61d6e90
karthik_kosaraju <Mohankarteek@gmail.com>
2024-03-22 11:46:49 -0400

    files shows the examples of  ArrayList

---
+++ src/Program1.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class Program1{
+
+    //Arraylist  = is nothing but a resizable array
+    //Elemnts can be added and removed after each co pilation phase,
+    //which are stored reference datatypes
+
+    public static void main(String[] args) {
+
+    ArrayList<String> food = new ArrayList<String>();
+
+    food.add("pizza");
+    food.add("hamburger");
+    food.add("hotdog");
+
+    food .set ( 2, "sushi");
+
+        for (int i= 0 ; i<food.size(); i++)
+            //normally for, arrays we use dot length parameter
+    //here for the arraylist i.e food size() parameter will be used
+        {
+            System.out.println("\n"+ food.get(i));
+         }
+
+    }
+}
---
+++ src/README.md
@@ -0,0 +1,1 @@
+THese are the examples for the ArrayList 
